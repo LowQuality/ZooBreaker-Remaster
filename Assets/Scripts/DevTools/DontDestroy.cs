7db3776ba532dcd2ad9a67831c931dd7823b3421
Kill00 <kill00@p.teamvery.dev>
2023-12-17 15:31:54 +0900

    1.1.6-0001 - Guid 시스템 추가 - DontDestroy가 작동하지 않던 버그 수정

--- Assets/Scripts/DevTools/DontDestroy.cs
+++ Assets/Scripts/DevTools/DontDestroy.cs
@@ -1,21 +1,29 @@
+using System.Collections.Generic;
+using Managements;
 using UnityEngine;
 
 namespace DevTools
 {
     public class DontDestroy : MonoBehaviour
     {
-        private DontDestroy _instance;
-        
+        private static readonly List<string> GuidList = new ();
         private void Awake()
         {
-            if (!_instance)
+            var guid = GuidManager.GetGuidByObject(gameObject);
+            if (guid == null)
             {
-                _instance = this;
-                DontDestroyOnLoad(gameObject);
+                Debug.LogError($"해당 게임 오브젝트 '{gameObject.name}'에는 GuidManager가 없기 때문에 DontDestroyOnLoad에 등록 할 수 없습니다!");
+                return;
+            }
+            
+            if (GuidList.Contains(guid))
+            {
+                Destroy(gameObject);
             }
             else
             {
-                Destroy(gameObject);
+                GuidList.Add(guid);
+                DontDestroyOnLoad(gameObject);
             }
         }
     }
---
+++ Assets/Scripts/Managements/GuidManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Managements
+{
+    public class GuidManager : MonoBehaviour
+    {
+        [SerializeField] [ReadOnly] internal string mGuid = System.Guid.NewGuid().ToString();
+        [HideInInspector] [SerializeField] internal string mLastGuid;
+        [SerializeField] internal bool dynamicGuid;
+
+        internal bool GameStarted;
+
+        public string Guid => mGuid;
+        public string LastGuid => mLastGuid;
+
+        private void Awake()
+        {
+            GameStarted = true;
+            if (dynamicGuid)
+            {
+                mGuid = System.Guid.NewGuid().ToString();
+            }
+        }
+
+        [ContextMenu("Generate new GUID")]
+        internal void GenerateNewGuid()
+        {
+            mGuid = System.Guid.NewGuid().ToString();
+        }
+
+        public static GameObject GetObjectByGuid(string guid)
+        {
+            var allObjects = FindObjectsOfType<GameObject>();
+            return (from obj in allObjects
+                let guidManager = obj.GetComponent<GuidManager>()
+                where guidManager != null && guidManager.Guid == guid
+                select obj).FirstOrDefault();
+        }
+        
+        public static string GetGuidByObject(GameObject obj)
+        {
+            var guidManager = obj.GetComponent<GuidManager>();
+            return guidManager != null ? guidManager.Guid : null;
+        }
+    }
+
+    [CustomEditor(typeof(GuidManager))]
+    public class GuidButton : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            if (GUILayout.Button("Generate new GUID"))
+            {
+                (target as GuidManager)?.GenerateNewGuid();
+            }
+
+            if (GUILayout.Button("Copy GUID"))
+            {
+                EditorGUIUtility.systemCopyBuffer = (target as GuidManager)?.Guid;
+            }
+
+            switch ((target as GuidManager)?.dynamicGuid)
+            {
+                case true when (target as GuidManager)?.GameStarted == false:
+                {
+                    if ((target as GuidManager)?.mLastGuid is "" or null) ((GuidManager)target).mLastGuid = (target as GuidManager)?.Guid;
+                    ((GuidManager)target).mGuid = "--- Dynamic GUID Mode Enabled ---";
+                    break;
+                }
+                case false when (target as GuidManager)?.GameStarted == false && (target as GuidManager)?.mGuid == "--- Dynamic GUID Mode Enabled ---":
+                    ((GuidManager)target).mGuid = (target as GuidManager)?.LastGuid;
+                    ((GuidManager)target).mLastGuid = null;
+                    break;
+            }
+        }
+    } 
+
+    public class ReadOnlyAttribute : PropertyAttribute
+    { }
+
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label);
+            GUI.enabled = true;
+        }
+    }
+}
